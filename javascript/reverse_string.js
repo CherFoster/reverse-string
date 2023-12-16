b7826dea20726f42c4617fae0db67bc5dd18341f
Cherise Foster <cherise.foster@outlook.com>
2023-12-16 12:44:55 -0700

    passed

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,15 @@
 function reverseString(str) {
-  // type your code here
+  let n = "";
+  for(let i = str.length-1; i>=0; i--){
+    n += str[i];
+  }
+  return n;
 }
+
+// create an empty new string
+// iterate over length of string backwards, where i is to the last index of the str
+// decrement i by 1 in each iteration
+// Concatenate the character at the current index 'i' of string 'str' to the variable 'n'
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,5 +24,8 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
+// faster way: split the string, reverse it, and join it back together
+// function reverseString(str){
+//   return str.split('').reverse().join('';)
+// }
